84b899cc5978cc8b32bf0020de7fecb0ebcd356b
yelasri <elasriyoussef604@gmail.com>
2025-12-05 16:46:18 +0100

    Entity

--- backend/src/main/java/com/blog/auth/model/AuthEntity.java
+++ backend/src/main/java/com/blog/auth/model/AuthEntity.java
@@ -1,5 +1,16 @@
 package com.blog.auth.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "users")
+@Entity
 public class AuthEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String username;
 }
--- backend/src/main/java/com/blog/auth/persistence/AuthRepository.java
+++ backend/src/main/java/com/blog/auth/persistence/AuthRepository.java
@@ -1,8 +1,11 @@
 package com.blog.auth.persistence;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.blog.auth.model.AuthEntity;
+
 @Repository
-public class AuthRepository {
-    
+public interface AuthRepository extends JpaRepository<AuthEntity, Long> {
+
 }
