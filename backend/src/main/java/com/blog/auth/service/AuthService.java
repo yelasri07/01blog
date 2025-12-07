46f2c514e9f215bf139c0027163a3c5fb4d01cfd
yelasri <elasriyoussef604@gmail.com>
2025-12-07 19:34:59 +0100

    spring security

--- backend/src/main/java/com/blog/auth/service/AuthService.java
+++ backend/src/main/java/com/blog/auth/service/AuthService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 
 import com.blog.auth.dto.LoginDTO;
 import com.blog.auth.dto.RegisterDTO;
-import com.blog.auth.security.JwtUtil;
 import com.blog.user.Model.UserEntity;
 import com.blog.user.persistence.UserRepository;
 
@@ -22,12 +21,6 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private JwtUtil jwtUtil;
-    @Autowired
-    AuthenticationManager authenticationManager;
-    @Autowired
-    PasswordEncoder encoder;
 
     public void createUser(RegisterDTO userData) throws Exception {
         if (userData.getUsername() == null) {
@@ -37,7 +30,7 @@ public class AuthService {
         UserEntity user = UserEntity.builder()
                 .username(userData.getUsername())
                 .email(userData.getEmail())
-                .password(encoder.encode(userData.getPassword()))
+                .password(userData.getPassword())
                 .created_at(Timestamp.valueOf("2004-04-06 12:40:10"))
                 .build();
 
@@ -45,15 +38,15 @@ public class AuthService {
     }
 
     public String loginUser(LoginDTO userData) throws UsernameNotFoundException {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        userData.getUsername(),
-                        userData.getPassword()));
+        // Authentication authentication = authenticationManager.authenticate(
+        //         new UsernamePasswordAuthenticationToken(
+        //                 userData.getUsername(),
+        //                 userData.getPassword()));
 
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String token = jwtUtil.generateToken(userDetails.getUsername());
+        // UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        // String token = jwtUtil.generateToken(userDetails.getUsername());
 
-        return token;
+        return "ok";
     }
 
 }
