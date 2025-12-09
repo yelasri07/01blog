54e679d0a157ae8c646ab581d8fd9a3c411a2d4f
yelasri <elasriyoussef604@gmail.com>
2025-12-09 22:52:49 +0100

    generate JWT

--- backend/src/main/java/com/blog/auth/security/JwtService.java
+++ backend/src/main/java/com/blog/auth/security/JwtService.java
@@ -1,20 +1,40 @@
 package com.blog.auth.security;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
     
     public String generateJwt(Long id, String username) {
         long currentTime = System.currentTimeMillis();
-        long expiredTime = currentTime + 30 * 60 * 1000;
+        long expirationTime = currentTime + 30 * 60 * 1000;
 
-        // Jwts.builder()
-        //         .setIssuedAt()
+        Map<String, String> claims = Map.of(
+            "username", username
+        );
 
-        return "dfsdfdsfdsa";
-    }
-     
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(currentTime))
+                .setExpiration(new Date(expirationTime))
+                .setSubject(String.valueOf(id))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+        }
+        
+        private Key getKey() {
+            return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+        }
 }
