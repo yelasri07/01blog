591461f64797663a56dadc51ab40205c666038c9
yelasri <elasriyoussef604@gmail.com>
2026-01-12 18:02:32 +0100

    add markdown

--- frontend/src/app/features/blogs/new-blog/new-blog.ts
+++ frontend/src/app/features/blogs/new-blog/new-blog.ts
@@ -1,9 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { marked } from 'marked';
+import { MarkdownComponent } from "ngx-markdown";
 
 @Component({
   selector: 'app-new-blog',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MarkdownComponent],
   templateUrl: './new-blog.html',
   styleUrl: './new-blog.scss',
 })
@@ -17,14 +19,21 @@ export class NewBlog {
     content: new FormControl('')
   })
 
-  blogResult() {
+  async blogResult() {
+    
     if (this.show()) {
       this.show.set(false)
       return;
     }
-
+    
     this.show.set(true)
+
+    let res = await marked.parse(this.content.value!, {
+      breaks: true,
+    });
+    console.log(res)
     if (this.title.value) this.resultTitle.set(this.title.value)
+    if (this.content.value) this.resultContent.set(res)
   }
 
   get title() {
