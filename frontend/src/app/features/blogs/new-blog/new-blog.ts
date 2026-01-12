f669838958346896374398cf644e5dc8a40a61cb
yelasri <elasriyoussef604@gmail.com>
2026-01-12 14:17:30 +0100

    add show result button in newBlog component

--- frontend/src/app/features/blogs/new-blog/new-blog.ts
+++ frontend/src/app/features/blogs/new-blog/new-blog.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-blog',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-blog.html',
   styleUrl: './new-blog.scss',
 })
 export class NewBlog {
+  resultTitle = signal('');
+  resultContent = signal('');
+  show = signal(false);
 
+  blogForm = new FormGroup({
+    title: new FormControl(''),
+    content: new FormControl('')
+  })
+
+  blogResult() {
+    if (this.show()) {
+      this.show.set(false)
+      return;
+    }
+
+    this.show.set(true)
+    if (this.title.value) this.resultTitle.set(this.title.value)
+  }
+
+  get title() {
+    return this.blogForm.controls.title
+  }
+
+  get content() {
+    return this.blogForm.controls.content
+  }
 }
