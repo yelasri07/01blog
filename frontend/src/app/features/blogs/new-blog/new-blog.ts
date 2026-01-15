c64c9884f1b18ead424d868769a55ebbaaab07f2
yelasri <elasriyoussef604@gmail.com>
2026-01-15 13:55:11 +0100

    add debounce to blog textarea

--- frontend/src/app/features/blogs/new-blog/new-blog.ts
+++ frontend/src/app/features/blogs/new-blog/new-blog.ts
@@ -1,7 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { marked } from 'marked';
 import { MarkdownComponent } from "ngx-markdown";
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-new-blog',
@@ -9,31 +10,67 @@ import { MarkdownComponent } from "ngx-markdown";
   templateUrl: './new-blog.html',
   styleUrl: './new-blog.scss',
 })
-export class NewBlog {
+export class NewBlog implements OnInit, OnDestroy {
   resultTitle = signal('');
   resultContent = signal('');
-  show = signal(false);
+  show = signal(true);
+
+  textarea = new Subject<Event>();
 
   blogForm = new FormGroup({
     title: new FormControl(''),
-    content: new FormControl('')
+    content: new FormControl(''),
+    file: new FormControl('')
   })
 
+  ngOnInit(): void {
+    this.textarea.pipe(
+      debounceTime(300)
+    ).subscribe(async (event) => {
+      const input = event.target as HTMLTextAreaElement
+      await this.addToMarkdown(input.value);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.textarea.complete();
+  }
+
+  handleSubmit() {
+
+  }
+
+  async handleTextareaChange(event: Event) {
+    if (!this.show()) return;
+    this.textarea.next(event)
+  }
+
   async blogResult() {
-    
     if (this.show()) {
       this.show.set(false)
       return;
     }
-    
+
     this.show.set(true)
 
-    let res = await marked.parse(this.content.value!, {
+    if (this.title.value !== null) this.resultTitle.set(this.title.value)
+    if (this.content.value !== null) await this.addToMarkdown(this.content.value)
+  }
+
+  uploadFile(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files) return;
+
+    const file = URL.createObjectURL(input.files[0])
+    console.log(file)
+  }
+
+  private async addToMarkdown(text: string) {
+    let res = await marked.parse(text, {
       breaks: true,
     });
-    console.log(res)
-    if (this.title.value) this.resultTitle.set(this.title.value)
-    if (this.content.value) this.resultContent.set(res)
+
+    this.resultContent.set(res)
   }
 
   get title() {
@@ -42,5 +79,9 @@ export class NewBlog {
 
   get content() {
     return this.blogForm.controls.content
+  }
+
+  get file() {
+    return this.blogForm.controls.file
   }
 }
